8e9e2c7163accbb1db182f20ec1d4843494b20d3
Simon <simon.kola@studenti.isissgobetti.it>
2026-03-27 12:46:55 +0100

    UI Polish: centered experience/projects and fixed navigation IDs

--- src/components/home/Leadership.jsx
+++ src/components/home/Leadership.jsx
@@ -1,6 +1,8 @@
+```javascript
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import { Jumbotron } from "./migration";
+import { Row } from "react-bootstrap"; // Added Row import
 
 const Leadership = ({ heading, message, img, imageSize }) => {
   return (
@@ -10,33 +12,37 @@ const Leadership = ({ heading, message, img, imageSize }) => {
       style={{ backgroundColor: "white" }}
     >
       <h2 className="display-4 pb-5 text-center">{heading}</h2>
-      <div className="row">
-        <div className="col-md-5">
+      <div className="row justify-content-center">
+        <div className="col-12 text-center">
           <p className="lead">{message}</p>
         </div>
-        <div className="col-md-7">
-          <Carousel>
-            {img.map((value, index) => {
-              return (
-                <Carousel.Item key={index}>
-                  <img
-                    className="d-block w-100"
-                    src={value.img}
-                    alt="First slide"
-                    width={imageSize.width}
-                    height={imageSize.height}
-                  />
-                  <Carousel.Caption>
-                    <h3>{value.label}</h3>
-                    <p>
-                      {value.paragraph}
-                    </p>
-                  </Carousel.Caption>
-                </Carousel.Item>
-              );
-            })}
-          </Carousel>
-        </div>
+        {img && img.length > 0 && (
+          <div className="col-md-7">
+            <Carousel>
+              {img.map((value, index) => {
+                return (
+                  <Carousel.Item key={index}>
+                    <img
+                      className="d-block w-100"
+                      src={value.img}
+                      alt="First slide"
+                      width={imageSize.width}
+                      height={imageSize.height}
+                    />
+                    <Carousel.Caption>
+                      <Row className="justify-content-center">
+                        <h3>{value.label}</h3>
+                      </Row>
+                      <p>
+                        {value.paragraph}
+                      </p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                );
+              })}
+            </Carousel>
+          </div>
+        )}
       </div>
     </Jumbotron>
   );
